eab03e0a17c6c051b5457e73264b2638718e33c3
Sebastian Wagner <wagner@cert.at>
2020-01-24 10:49:54 +0100

    update-vm: also install landing page

--- update-vm.sh
+++ update-vm.sh
@@ -8,3 +8,6 @@ sudo systemctl start rabbitmq-server.service
 rabbitmqctl set_permissions intelmq ".*" ".*" ".*"
 rabbitmqctl set_permissions admin ".*" ".*" ".*"
 systemctl stop rabbitmq-server.service
+
+# install new landing page
+sudo cp /home/user/intelmq-tutorial/ansible/files/landingpage.html /var/www/html/index.html
