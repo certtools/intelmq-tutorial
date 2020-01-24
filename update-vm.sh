946714c55557d1625a1de976c9d4b6f27d40b16e
Sebastian Wagner <wagner@cert.at>
2020-01-24 12:46:57 +0100

    update-vm: various fixes

--- update-vm.sh
+++ update-vm.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 # wrong permissions on state file
-sudo chown intelmq:intelmq /opt/intelmq/var/lib/state.conf
+sudo chown intelmq:intelmq /opt/intelmq/var/lib/state.json
 
 # missing permissions in rabbitmq
 sudo systemctl start rabbitmq-server.service
-rabbitmqctl set_permissions intelmq ".*" ".*" ".*"
-rabbitmqctl set_permissions admin ".*" ".*" ".*"
-systemctl stop rabbitmq-server.service
+sudo rabbitmqctl set_permissions intelmq ".*" ".*" ".*"
+sudo rabbitmqctl set_permissions admin ".*" ".*" ".*"
+sudo systemctl stop rabbitmq-server.service
 
 # install new landing page
 sudo cp /home/user/intelmq-tutorial/ansible/files/landingpage.html /var/www/html/index.html
+
+# install update-vm script
+sudo cp /home/user/intelmq-tutorial/update-vm.sh /usr/local/bin/
+chmod +x /usr/local/bin/update-vm.sh
